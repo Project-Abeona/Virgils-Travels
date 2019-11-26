dc920e7b5ae4babb9ebf8a106a934687396bba96
Hector Mendoza <44214086+hmendoza23@users.noreply.github.com>
2019-11-26 14:04:40 -0800

    Changes to about, accessibility, contact, cookies, team, TOS, added resized images for "meet the team", added CardUI, added card-style

--- front-end/src/components/Footer Links/About.js
+++ front-end/src/components/Footer Links/About.js
@@ -6,8 +6,27 @@ class About extends Component {
 
         <div className="container">
           <br/><br/>
+          <div class = "centering" data-analytics-section = "Center">
+            <div class = "block center">
+              <h1 class = "h1"><center>About us</center></h1>
+              <hr></hr>
+              <p>
+              <center>Our application, Virgil’s Travels, aims to provide an interactive, all encompassing 
+                travel site that provides users with various options other sites may lack. We strive to make 
+                our application as social as possible by having users create profiles and see what other 
+                users have purchased, reviewed, and shared about their trips. The site itself was created by
+                a group of 5 computer science students attending California State University Long Beach.
+                Our team consists of David Ibarra, Bryan Rojas, Clayton Asada, Hector Mendoza, and 
+                Brandon Mitchell. This site is still in development, and we hope to see some great improvement
+                coming in the near future. Thanks for checking out our about us! We hope you stick around to
+                see our website progress! </center>
+              </p>
+              
+            </div>
+          </div>
           
-          Write about us and html goes here
+          
+         
 
           <br/><br/><br/>
         </div>
